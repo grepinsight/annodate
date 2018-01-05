52218c8ae04724271875175405d0d0195fa5066e
Albert Lee <grepinsight@gmail.com>
2018-01-05 07:43:47 +0000

    Implement annodate

---
+++ annodate.go
@@ -0,0 +1,74 @@
+// annodate prepends/appends day of week to stdin or file
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"time"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func annodate(r io.Reader, delim string, field int, last bool) {
+
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		line := scanner.Text() // Println will add back the final '\n'
+		fields := strings.Split(line, delim)
+
+		dateField := fields[field-1]
+		// change formt
+		t, _ := time.Parse("2006-01-02", dateField)
+		delim := delim
+		if last {
+			fmt.Printf("%s%s%s\n", line, delim, t.Format("Monday"))
+		} else {
+			fmt.Printf("%s%s%s\n", t.Format("Monday"), delim, line)
+		}
+
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+}
+
+func stdinOrFile() io.Reader {
+
+	fi, err := os.Stdin.Stat()
+	check(err)
+
+	if fi.Mode()&os.ModeNamedPipe != 0 {
+		return os.Stdin
+	}
+	// If nothing is given produce help message
+	if len(os.Args) == 1 {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	// Assume os.Args[1] is a file
+	file, err := os.Open(os.Args[1])
+	check(err)
+	return file
+
+}
+
+func main() {
+
+	// func String(name string, value string, usage string) *string {
+	delimPtr := flag.String("d", " ", "Delimiter")
+	fieldPtr := flag.Int("f", 1, "Field that contains date data")
+	lastColPtr := flag.Bool("e", false, "Add column at the end?")
+	// formatPtr := flag.String("t", "2006-01-02", "Format")
+	flag.Parse()
+
+	annodate(stdinOrFile(), *delimPtr, *fieldPtr, *lastColPtr)
+}
