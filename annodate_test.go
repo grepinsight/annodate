f5fac99b069091473aa537a75577be1df66865d2
Albert Lee <grepinsight@gmail.com>
2018-01-06 21:25:02 -0500

    Add another test case

--- annodate_test.go
+++ annodate_test.go
@@ -5,9 +5,15 @@ import (
 )
 
 func TestProcLine(t *testing.T) {
-	got := procLine("2017-01-01,new year", ",", 1, false)
-	want := "Sunday,2017-01-01,new year"
+	got := procLine("2018-01-01,new year", ",", 1, false)
+	want := "Monday,2018-01-01,new year"
 	if got != want {
 		t.Errorf("expecting %q, got %q", want, got)
+	}
+
+	got2 := procLine("new year\t2018-01-01", "\t", 1, true)
+	want2 := "new year\t2018-01-01\tMonday"
+	if got2 != want2 {
+		t.Errorf("expecting %q, got %q", want2, got2)
 	}
 }
